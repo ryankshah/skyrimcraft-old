ae5e9f3a5de40eda085f916f0c815883b53628bb
Ryan Shah <ryan.k.shah@gmail.com>
2021-06-13 09:41:45 +0100

    map updates

--- src/main/java/com/ryankshah/skyrimcraft/client/gui/MapScreen.java
+++ src/main/java/com/ryankshah/skyrimcraft/client/gui/MapScreen.java
@@ -2,17 +2,27 @@ package com.ryankshah.skyrimcraft.client.gui;
 
 import com.mojang.blaze3d.matrix.MatrixStack;
 import com.mojang.blaze3d.systems.RenderSystem;
+import com.ryankshah.skyrimcraft.character.ISkyrimPlayerData;
+import com.ryankshah.skyrimcraft.character.ISkyrimPlayerDataProvider;
+import com.ryankshah.skyrimcraft.util.CompassFeature;
+import it.unimi.dsi.fastutil.objects.ObjectArrayList;
+import it.unimi.dsi.fastutil.objects.ObjectList;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.entity.player.ClientPlayerEntity;
 import net.minecraft.client.gui.screen.Screen;
 import net.minecraft.client.renderer.RenderType;
+import net.minecraft.client.renderer.WorldRenderer;
 import net.minecraft.client.renderer.texture.AtlasTexture;
-import net.minecraft.util.math.vector.Matrix4f;
+import net.minecraft.util.Direction;
 import net.minecraft.util.math.vector.Quaternion;
 import net.minecraft.util.math.vector.Vector3d;
 import net.minecraft.util.math.vector.Vector3f;
 import net.minecraft.util.text.TranslationTextComponent;
 import net.minecraft.world.World;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class MapScreen extends Screen
 {
@@ -20,14 +30,33 @@ public class MapScreen extends Screen
     private ClientPlayerEntity player;
     private World world;
     private Vector3d camera;
+    private ObjectList<WorldRenderer.LocalRenderInformationContainer> renderChunks;
+    private ObjectList<WorldRenderer.LocalRenderInformationContainer> mapChunks;
+    private ISkyrimPlayerData playerCap;
+    private List<CompassFeature> features;
 
     protected MapScreen() {
         super(new TranslationTextComponent(".mapgui.title"));
 
         this.minecraft = Minecraft.getInstance();
+
+        // minecraft.options.renderDistance = 64;
+
         this.player = Minecraft.getInstance().player;
         this.world = player.level;
         this.camera = minecraft.gameRenderer.getMainCamera().getPosition();
+        this.playerCap = player.getCapability(ISkyrimPlayerDataProvider.SKYRIM_PLAYER_DATA_CAPABILITY).orElseThrow(() -> new IllegalStateException("Skills Screen capability failed"));
+
+        this.renderChunks = new ObjectArrayList<>(69696);
+        this.mapChunks = new ObjectArrayList<>(69696);
+
+        this.mapChunks.addAll(Arrays.stream(
+                this.minecraft.levelRenderer.viewArea.chunks
+        ).map(
+                chunkRender -> minecraft.levelRenderer.new LocalRenderInformationContainer(chunkRender, (Direction)null, 0)
+        ).collect(Collectors.toList()));
+
+        //this.features = playerCap.getCompassFeatures().stream().filter(feature -> feature.getChunkPos().equals())
     }
 
     @Override
@@ -39,64 +68,51 @@ public class MapScreen extends Screen
     public void render(MatrixStack matrixStack, int mouseX, int mouseY, float partialTicks) {
         RenderSystem.color4f(1.0F, 1.0F, 1.0F, 1.0F);
 
-        this.renderBackground(matrixStack);
+        if(world != null) {
+            fill(matrixStack, 0, 0, this.width, this.height, 0xFF000000);
+            net.minecraftforge.common.MinecraftForge.EVENT_BUS.post(new net.minecraftforge.client.event.GuiScreenEvent.BackgroundDrawnEvent(this, matrixStack));
+        }
 
-        //RenderSystem.enableDepthTest();
+        this.renderChunks = minecraft.levelRenderer.renderChunks;
+
+        this.minecraft.levelRenderer.renderChunks.clear();
+        this.minecraft.levelRenderer.renderChunks.addAll(mapChunks);
 
         // Set d1 to 200 or max build height...
-        double d0 = 0, d1 = 200d, d2 = 0;
+        double d0 = player.blockPosition().getX(), d1 = 200d, d2 = player.blockPosition().getZ();
 
-        //d0 = 0D;
-        //d1 = 0D;
-        //d2 = 0D;
+        //Matrix4f matrix4f1 = matrixStack.last().pose();
+        this.resetProjectionMatrix();
+        matrixStack.translate(width / 2, height / 2 + 45f, -1.4143 * minecraft.options.renderDistance*16);
 
-        Matrix4f matrix4f1 = matrixStack.last().pose();
-        this.resetProjectionMatrix(matrix4f1);
-
-        // getMain
-        float width = minecraft.getMainRenderTarget().viewWidth;
-        float height = minecraft.getMainRenderTarget().viewHeight;
-
-        Matrix4f matrix4f2 = Matrix4f.orthographic(width / height, 1, 0.1f, 1000.0f);
-        //this.resetProjectionMatrix(matrix4f2);
         matrixStack.pushPose();
-        matrixStack.last().pose().set(matrix4f2);
-        matrixStack.pushPose();
-        //        ClippingHelper clippinghelper = new ClippingHelper(matrix4f2, matrix4f1);
-//        clippinghelper.prepare(d0, d1, d2);
 
-        //matrixStack.mulPose(new Quaternion(Vector3f.XN.rotationDegrees(-22.5f)));
         matrixStack.mulPose(new Quaternion(Vector3f.XP.rotationDegrees(60f)));
-        //matrixStack.mulPose(new Quaternion(Vector3f.YP.rotationDegrees(45f)));
+        matrixStack.mulPose(new Quaternion(Vector3f.YP.rotationDegrees(-22.5f)));
 
-        //matrixStack.translate(0, 0, 0);
-
-        matrixStack.pushPose();
-        matrixStack.scale(1/100F, 1/100F, 1/100F);
-        //matrixStack.mulPose(new Quaternion(Vector3f.YP.rotationDegrees(-45f)));
-        //matrixStack.translate(0, 0, 0);
         minecraft.levelRenderer.renderChunkLayer(RenderType.solid(), matrixStack, d0, d1, d2);
         minecraft.getModelManager().getAtlas(AtlasTexture.LOCATION_BLOCKS).setBlurMipmap(false, this.minecraft.options.mipmapLevels > 0); // FORGE: fix flickering leaves when mods mess up the blurMipmap settings
         minecraft.levelRenderer.renderChunkLayer(RenderType.cutoutMipped(), matrixStack, d0, d1, d2);
         minecraft.getModelManager().getAtlas(AtlasTexture.LOCATION_BLOCKS).restoreLastBlurMipmap();
         minecraft.levelRenderer.renderChunkLayer(RenderType.cutout(), matrixStack, d0, d1, d2);
+        minecraft.levelRenderer.renderChunkLayer(RenderType.translucent(), matrixStack, d0, d1, d2);
         //RenderHelper.setupLevel(matrixStack.last().pose());
 
-        matrixStack.popPose();
-        matrixStack.popPose();
         matrixStack.popPose();
 
         //RenderSystem.disableDepthTest();
 
+        this.minecraft.levelRenderer.renderChunks.clear();
+        this.minecraft.levelRenderer.renderChunks.addAll(this.renderChunks);
+
         super.render(matrixStack, mouseX, mouseY, partialTicks);
     }
 
-    public void resetProjectionMatrix(Matrix4f p_228379_1_) {
+    public void resetProjectionMatrix() {
         RenderSystem.matrixMode(5889); // projection view
         RenderSystem.loadIdentity();
-        RenderSystem.multMatrix(p_228379_1_);
+        RenderSystem.ortho(0, width, 0, height, 1f, 1000.0f);
         RenderSystem.matrixMode(5888); // model view
         RenderSystem.loadIdentity();
-        RenderSystem.multMatrix(p_228379_1_);
     }
 }
